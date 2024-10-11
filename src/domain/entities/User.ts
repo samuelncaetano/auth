ab151bec8b1d1c615aeee30a2e0fd60cea29e572
samuelncaetano <samueln.caetano42@gmail.com>
2024-10-11 17:12:41 -0300

    feat(entities): add the user entity

---
+++ src/domain/entities/User.ts
@@ -0,0 +1,17 @@
+export interface IUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export class User {
+  constructor(
+    public readonly name: string,
+    public readonly email: string,
+    private readonly password: string,
+  ) {}
+
+  getPassword(): string {
+    return this.password;
+  }
+}
