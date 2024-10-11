4d69103f5f40b192e40df280ce141166780ff210
samuelncaetano <samueln.caetano42@gmail.com>
2024-10-11 17:57:59 -0300

    feat(config): add the dependency injections to create the user

---
+++ src/main/config/dependencyInjection/userDependencyInjection.ts
@@ -0,0 +1,14 @@
+import { BcryptPassword } from "@/application/utils/hashUtils";
+import { CreateUserController } from "@/main/controllers/user/createUserController";
+import { CreateUserUseCase } from "@/application/useCases/createUseCases";
+import { PrismaAuthUser } from "@/infrastructure/database/prisma/prismaAuthUser";
+import { UserFactory } from "@/domain/factories/userFactory";
+
+// Create
+const bcryptPassword = new BcryptPassword();
+const userFactory = new UserFactory(bcryptPassword);
+const prismaAuthUser = new PrismaAuthUser();
+const createUserUseCase = new CreateUserUseCase(userFactory, prismaAuthUser);
+const createUserController = new CreateUserController(createUserUseCase);
+
+export { createUserController };
