cfb099097df4e548f969fae5fea4dd95af0b13ed
samuelncaetano <samueln.caetano42@gmail.com>
2024-10-12 10:58:35 -0300

    feat(config): add dependency injections for user auth

--- src/main/config/dependencyInjection/userDependencyInjection.ts
+++ src/main/config/dependencyInjection/userDependencyInjection.ts
@@ -1,14 +1,27 @@
 import { BcryptPassword } from "@/application/utils/hashUtils";
 import { CreateUserController } from "@/main/controllers/user/createUserController";
 import { CreateUserUseCase } from "@/application/useCases/createUseCases";
+import { JWTTokenGenerator } from "@/application/utils/authUtils";
+import { LoginUserController } from "@/main/controllers/user/loginUserController";
+import { LoginUserUseCase } from "@/application/useCases/loginUserUseCase";
 import { PrismaAuthUser } from "@/infrastructure/database/prisma/prismaAuthUser";
 import { UserFactory } from "@/domain/factories/userFactory";
 
-// Create
 const bcryptPassword = new BcryptPassword();
 const userFactory = new UserFactory(bcryptPassword);
 const prismaAuthUser = new PrismaAuthUser();
+const jWTTokenGenerator = new JWTTokenGenerator();
+
+// Create
 const createUserUseCase = new CreateUserUseCase(userFactory, prismaAuthUser);
 const createUserController = new CreateUserController(createUserUseCase);
 
-export { createUserController };
+// Login
+const loginUserUseCase = new LoginUserUseCase(
+  userFactory,
+  jWTTokenGenerator,
+  prismaAuthUser,
+);
+const loginUserController = new LoginUserController(loginUserUseCase);
+
+export { createUserController, loginUserController };
